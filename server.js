a53cd807a399c10652e26f25ac28912fddc55fef
Peter <neetisthebest@gmail.com>
2017-05-18 21:40:57 -0700

    add project page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req,res)=>{
     pageTitle: 'About Page'
   })
 });
+
+app.get('/project', (req,res)=>{
+  res.render('project.hbs',{
+    pageTitle: 'Project Page'
+  })
+});
 app.get('/bad',(req,res)=>{
   res.send({
     errorMessage: 'error'
